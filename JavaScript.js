b6a9cdbbadb8ce11333e2b89e1cd9423509cd273
PC141312\mi141312 <takumihaisai123@gmail.com>
2017-09-02 11:23:29 +0900

    top, images section completed

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  
+  var position = $(".section-margin").offset().top;
+
+  $(".scroll-button").click(function(){
+    $("html,body").animate({
+        scrollTop : position
+    }, {
+        queue : false
+    });
+  });
 });
